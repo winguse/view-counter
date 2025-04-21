5db5cfc2a6b04385add0c50e4f1b9404260e3747
Yingyu Cheng <1443504+winguse@users.noreply.github.com>
2025-04-21 01:29:19 +0000

    multi page support

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,57 @@
 import { StyleOption, badgen } from 'badgen';
 import {
-  Request as WorkerRequest,
-  D1Database,
-  ExecutionContext
+	Request as WorkerRequest,
+	D1Database,
 } from '@cloudflare/workers-types/experimental';
 
 /**
  * Environment variables type definition.
  */
 export interface Env {
-  ViewCounter: D1Database;
+	ViewCounter: D1Database;
 }
 
-/**
- * The name of the counter to use.
- * Feel free to change this to a unique name for your own counter.
- */
-const CounterName = 'Counter1';
-
 export default {
-  /**
-   * Fetches the view count and generates an SVG badge.
-   * @param request The incoming request.
-   * @param env The environment variables.
-   * @returns A response containing the SVG badge.
-   */
-  async fetch(request: WorkerRequest, env: Env): Promise<Response> {
-    // Step the count
-    let count: number | null = await getCountFromD1(env);
-    count = await stepCount(count, env);
-    console.log('Count', count);
+	/**
+	 * Fetches the view count and generates an SVG badge.
+	 * @param request The incoming request.
+	 * @param env The environment variables.
+	 * @returns A response containing the SVG badge.
+	 */
+	async fetch(request: WorkerRequest, env: Env): Promise<Response> {
+		const { searchParams } = new URL(request.url);
+		const referer = request.headers.get('referer');
+		let id = searchParams.get('r') || '/';
+		if (referer && id === '/') {
+			// Extract the path from the referer URL
+			const { host, pathname } = new URL(referer);
+			id = host + pathname;
+		}
 
-    // Get the query parameters
-    const params = readQueryParams(request);
-    console.log('Params', params);
+		// Step the count
+		let count: number | null = await getCountFromD1(env, id);
+		count = await stepCount(id, count, env);
+		console.log('Count', count);
 
-    // Generate the svg string
-    const svgString = badgen({
-      ...params,
-      status: count.toString()
-    });
-    console.log('SVG', svgString);
+		// Get the query parameters
+		const params = readQueryParams(searchParams);
+		console.log('Params', params);
 
-    return new Response(svgString, {
-      headers: {
-        'content-type': 'image/svg+xml;charset=utf-8',
-        'access-control-allow-origin': '*',
-        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0'
-      }
-    });
-  }
+		// Generate the svg string
+		const svgString = badgen({
+			...params,
+			status: count.toString()
+		});
+		console.log('SVG', svgString);
+
+		return new Response(svgString, {
+			headers: {
+				'content-type': 'image/svg+xml;charset=utf-8',
+				'access-control-allow-origin': '*',
+				'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0'
+			}
+		});
+	}
 };
 
 /**
@@ -57,61 +59,59 @@ export default {
  * @param env The environment variables.
  * @returns The current count, or null if it doesn't exist.
  */
-const getCountFromD1 = async (env: Env): Promise<number | null> =>
-  await env.ViewCounter.prepare('SELECT * FROM ViewCounter WHERE Name = ?1')
-    .bind(CounterName)
-    .first('Value');
+const getCountFromD1 = async (env: Env, id: string): Promise<number | null> =>
+	await env.ViewCounter.prepare('SELECT view_count FROM view_counter WHERE id = ?')
+		.bind(id)
+		.first('view_count');
 
 /**
  * Increments the count and updates the ViewCounter.
+ * @param id The ID of the view counter.
  * @param count The current count.
  * @param env The environment variables.
  * @returns The new count.
  */
-const stepCount = async (count: number | null, env: Env): Promise<number> => {
-  // If the value is null, insert a new record
-  if (!count) {
-    const result = await env.ViewCounter.prepare(
-      'INSERT INTO ViewCounter (Name, Value) VALUES (?1, 1)'
-    )
-      .bind(CounterName)
-      .run();
+const stepCount = async (id: string, count: number | null, env: Env): Promise<number> => {
+	// If the value is null, insert a new record
+	if (!count) {
+		const result = await env.ViewCounter.prepare(
+			'INSERT INTO view_counter (id, view_count) VALUES (?, ?)'
+		)
+			.bind(id, 1)
+			.run();
+		console.log('Insert result', result);
+	} else {
+		// Step the count in the database
+		const result = await env.ViewCounter.prepare(
+			'UPDATE view_counter SET view_count = view_count + 1 WHERE id = ?'
+		)
+			.bind(id)
+			.run();
+		console.log('Update result', result);
+	}
 
-    console.log('Insert result', result);
-  } else {
-    // Step the count in the database
-    const result = await env.ViewCounter.prepare(
-      'UPDATE ViewCounter SET Value = (SELECT Value FROM ViewCounter WHERE Name = ?1) + 1 WHERE Name = ?1'
-    )
-      .bind(CounterName)
-      .run();
-
-    console.log('Update result', result);
-  }
-
-  return (count || 0) + 1;
+	return (count || 0) + 1;
 };
 
 /**
  * Reads the query parameters from the request.
- * @param request The incoming request.
+ * @param searchParams URLSearchParams
  * @returns The query parameters.
  */
 const readQueryParams: (
-  request: WorkerRequest
+	searchParams: URLSearchParams
 ) => {
-  label: string;
-  labelColor: string;
-  color: string;
-  style: StyleOption;
-  scale: number;
-} = request => {
-  const { searchParams } = new URL(request.url);
-  return {
-    label: searchParams.get('label') || 'Views',
-    labelColor: searchParams.get('labelColor') || '555',
-    color: searchParams.get('color') || 'blue',
-    style: searchParams.get('style') === 'classic' ? 'classic' : 'flat',
-    scale: parseFloat(searchParams.get('scale') || '1')
-  };
+	label: string;
+	labelColor: string;
+	color: string;
+	style: StyleOption;
+	scale: number;
+} = searchParams => {
+	return {
+		label: searchParams.get('label') || 'Views',
+		labelColor: searchParams.get('labelColor') || '555',
+		color: searchParams.get('color') || 'blue',
+		style: searchParams.get('style') === 'classic' ? 'classic' : 'flat',
+		scale: parseFloat(searchParams.get('scale') || '1')
+	};
 };
